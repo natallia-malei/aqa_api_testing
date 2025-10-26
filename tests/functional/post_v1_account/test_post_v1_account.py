689b529bc7a950030a070b506f0f37e9e3e9335f
Natallia Malei <natalliamalei@gmail.com>
2025-10-26 10:13:49 +0300

    фикс имя пользователя

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -11,8 +11,8 @@ def test_v1_account():
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    login = 'nm_test6'
-    email = 'nmtest6@gmail.com'
+    login = 'nm_test7'
+    email = f'{login}@gmail.com'
     password = '12345679'
     json_data = {
         'login': login,
