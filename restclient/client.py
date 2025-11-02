9dab2b633fa8b5cc9ad7b4b0e86469b671a63e95
Natallia Malei <natalliamalei@gmail.com>
2025-11-02 18:54:43 +0300

    Добавили конфигурацию в RestClient

--- restclient/client.py
+++ restclient/client.py
@@ -6,10 +6,17 @@ from requests import (
 import structlog
 import uuid
 
+from restclient.configuration import Configuration
+
+
 class RestClient:
-    def __init__(self, host, headers=None):
-        self.host = host
-        self.headers = headers
+    def __init__(
+            self,
+            configuration: Configuration
+        ):
+        self.host = configuration.host
+        self.headers = configuration.headers
+        self.disable_log = configuration.disable_log
         self.session = session()
         self.log = structlog.getLogger(__name__).bind(service='api')
 
@@ -47,6 +54,9 @@ class RestClient:
     def _send_request(self, method, path, **kwargs):
         log = self.log.bind (event_id=str(uuid.uuid4())) # для получения по логам уникального id
         full_url = self.host + path
+        if self.disable_log:
+            rest_response = self.session.request(method=method, url=full_url, **kwargs)
+            return rest_response
 
         log.msg(
             event='Request',
@@ -58,7 +68,6 @@ class RestClient:
             data=kwargs.get('data')
         )
         rest_response = self.session.request(method=method, url=full_url, **kwargs)
-
         #pip install curlify
         curl = curlify.to_curl(rest_response.request)
         print(curl)
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -3,6 +3,10 @@ from json import loads
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhoog.apis.mailhog_api import MailhogApi
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
+
+
 # импорт настроек логов, где можем указать вид логов
 import structlog
 
@@ -19,11 +23,14 @@ structlog.configure(
 
 def test_v1_account():
     # Регистрация пользователя
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
 
-    login = 'nm_test20'
+    account_api = AccountApi(configuration=dm_api_configuration)
+    login_api = LoginApi(configuration=dm_api_configuration)
+    mailhog_api = MailhogApi(configuration=mailhog_configuration)
+
+    login = 'nm_test24'
     email = f'{login}@gmail.com'
     password = '12345679'
     json_data = {
